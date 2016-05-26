2d247155ba137c98f4f3899141460724a5c75b63
Xavier Sala <utrescu@gmail.com>
2016-05-26 17:06:06 +0200

    Javascript: icona preview i last tab en perfil

---
+++ src/main/resources/static/js/icon.js
@@ -0,0 +1,29 @@
+/**
+ * Previsualitza la icona de l'usuari.
+ */
+
+$(document).ready(function() {
+	// Canvia la icona quan es carrega una imatge nova
+	$("#icona").change(function() {
+		var oFReader = new FileReader();
+		oFReader.readAsDataURL(this.files[0]);		
+		oFReader.onload = function(carregaEvent) {
+			$('#profile').attr("src", carregaEvent.target.result);
+		};
+	});
+	
+	// Recorda la darrera pestanya
+	$(function() { 
+	    // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
+	    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	        // save the latest tab; use cookies if you like 'em better:
+	        localStorage.setItem('lastTab', $(this).attr('href'));
+	    });
+
+	    // go to the latest tab, if it exists:
+	    var lastTab = localStorage.getItem('lastTab');
+	    if (lastTab) {
+	        $('[href="' + lastTab + '"]').tab('show');
+	    }
+	});	
+});
